c87433841fe640b93a86619e9deeb82c72e623a2
Marcus VS Maziero <marcus.maziero@outlook.com>
2023-07-07 14:46:34 -0300

    Deixa 2 opções de classificadores

--- classification-svm.py
+++ classification-svm.py
@@ -7,6 +7,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.cluster import KMeans
 from sklearn.metrics import classification_report, confusion_matrix
 from mlxtend.plotting import plot_confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from progress.bar import Bar
 import glob
@@ -25,7 +26,7 @@ def getImages(file):
     blurImage = cv2.medianBlur(grayImage,3)
     return blurImage
 
-#Como esta dividido em 2 pastas diferentes não é validado o label, sendo BIKE 0 e CAR 1
+#Como esta dividido em 2 pastas diferentes não é validado o label, sendo BIKE 0 e CAR 1 - até porque temos algumas imagens que não estão com o fileName correto mas estão na pasta certa
 def readData(path):
     patterns = ["*.*"]
     images = []
@@ -68,20 +69,20 @@ def find_index(descriptor, center):
 
 def getHistogramList(descriptorList, centers):
     histogramList = []
-    bar = Bar('[INFO] Extraindo SIFT descritor...',suffix='Duration:%(elapsed)ds')
+    print('[INFO] Extraindo SIFT descritor...')
     for i in range(len(descriptorList)):
         histogram = np.zeros(len(centers))
         for d in range(len(descriptorList[i])):
             descriptor = descriptorList[i][d]
             idx = find_index(descriptor,centers)
             histogram[idx] += 1
-        bar.finish()
         histogramList.append(histogram)
+    print('[INFO] Finalizou a extração SIFT descritor...')
     return histogramList
 
 
 
-def training(scaler):
+def training(scaler, classifier):
     print('[INFO] Iniciando leitura para treinamento...')
     trainImages, trainLabels = readData(trainPath)
 
@@ -99,8 +100,8 @@ def training(scaler):
     print('[INFO] Capturando o historiograma para a base de treinamento...')
     trainData = getHistogramList(trainSiftDescriptors,centers)
 
-    print('[INFO] Treinando no modelo SVM...')
-    clf = make_pipeline(scaler, SVC(gamma='auto'))
+    print('[INFO] Treinando no modelo Escolhido...')
+    clf = make_pipeline(scaler, classifier)
     clf.fit(trainData, trainLabels)
     return clf, centers
 
@@ -125,11 +126,11 @@ def test(clf, centers):
 
     class_names = [ 'Bike','Car']
  
-    fig, ax = plot_confusion_matrix(conf_mat=conf_matrix, figsize=(6, 6), cmap=plt.cm.Greens)
+    plot_confusion_matrix(conf_mat=conf_matrix, figsize=(6, 6), cmap=plt.cm.Greens)
     plt.xlabel('Predictions', fontsize=18)
     plt.ylabel('Actuals', fontsize=18)
     plt.title('Confusion Matrix', fontsize=18)
-    plt.xticks(np.arange(2), class_names)  # rotation is optional
+    plt.xticks(np.arange(2), class_names)
     plt.yticks(np.arange(2), class_names)
     plt.show()
     print(conf_matrix)
@@ -138,7 +139,7 @@ def test(clf, centers):
 #EXECUÇÃO MAIN
 
 print ("Vamos iniciar o processamento de imagens da base BIKE and CAR um dataset com um total de 4.000 imagens, sendo metade de carros e metade de motos \n A base esta divida em 80% treinamento e 20% teste (3.600 - treinamento / 400 teste)")
-print ("Digite qual opção de normalização você deseja aplicar para o conjunto de treinamento: \n 1 = MinMaxScaler, 2 = StandardScaler, 3 = MaxAbsScaler, 4 = RobustScaler \n Por padrão utilizamos o MinMaxScaler")
+print ("Digite qual opção de normalização você deseja aplicar para o conjunto de treinamento: \n 1 = MinMaxScaler \n 2 = StandardScaler \n 3 = MaxAbsScaler \n 4 = RobustScaler")
 
 # Escolha umas das 4 técnicas de normalização existentes
 # 1 = MinMaxScaler, 2 = StandardScaler, 3 = MaxAbsScaler, 4 = RobustScaler
@@ -153,7 +154,16 @@ if selectedNormalization == 3:
 if selectedNormalization == 4:
     scaler = preprocessing.RobustScaler()
 
-clf, centers = training(scaler)
+print("Certo agora vamos escolher o Classificador: \n 1 = SVM \n 2 = KNN")
+selectedClassifier = int(input())
+
+if selectedClassifier == 1:
+    classifier = SVC(gamma='auto')
+if selectedClassifier == 2:
+    classifier = KNeighborsClassifier(n_neighbors = 3)
+
+
+clf, centers = training(scaler, classifier)
 test(clf, centers)
 
     
